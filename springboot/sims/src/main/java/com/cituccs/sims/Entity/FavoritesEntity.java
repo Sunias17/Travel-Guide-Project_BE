ba9d852699edd02a538b726b720db32a5122dcb8
Sunias17 <106364543+Sunias17@users.noreply.github.com>
2022-11-28 12:22:23 +0800

    Update FavoritesEntity.java

--- springboot/sims/src/main/java/com/cituccs/sims/Entity/FavoritesEntity.java
+++ springboot/sims/src/main/java/com/cituccs/sims/Entity/FavoritesEntity.java
@@ -8,8 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
 
 @Entity
 @Table(name="tbl_favorites")
